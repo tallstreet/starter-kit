fb0ff87a93f810261af76745c3c161f865a4458a
Gary Roberts <gary.roberts@smartfocus.com>
2015-07-19 18:26:08 +0100

    Update starter kit

--- app/components/Application/index.jsx
+++ app/components/Application/index.jsx
@@ -1,23 +1,25 @@
-import React from "react";
-import Header from "../Header";
+import React from 'react';
+import Header from '../Header';
 
 /*
   Component specific stylesheet
   Can also use .less, .scss, or plain .css files here
 */
-require("./style.scss");
+require('./style.scss');
 
 export default class Application extends React.Component {
   render() {
-    return <div className="ApplicationComponent">
-      <div className="ApplicationComponent-wrap">
-        <Header />
+    return (
+      <div className="ApplicationComponent">
+        <div className="ApplicationComponent-wrap">
+          <Header />
 
-        <main className="ApplicationComponent-body">
-          <p>Seems like creating your own React starter kit is a right of passage. So, here"s mine.</p>
-          <p>For more information, see the <a href="https://github.com/tallstreet/starter-kit#tallstret-starterkit">README</a>.</p>
-        </main>
+          <main className="ApplicationComponent-body">
+            <p>Seems like creating your own React starter kit is a right of passage. So, here"s mine.</p>
+            <p>For more information, see the <a href="https://github.com/tallstreet/starter-kit#tallstret-starterkit">README</a>.</p>
+          </main>
+        </div>
       </div>
-    </div>;
+    );
   }
 }
--- app/components/Header/index.jsx
+++ app/components/Header/index.jsx
@@ -1,26 +1,28 @@
-import React from "react";
+import React from 'react';
 
 /*
   Component specific stylesheet
   Can also use .less, .scss, or plain .css files here
 */
-require("./style.sass");
+require('./style.sass');
 
 /*
   Reference an image and get back a URL automatically via webpack.
   webpack takes care of versioning, bundling for production, etc.
 */
-const logoURL = require("./images/react-logo.svg");
+const logoURL = require('./images/react-logo.svg');
 
 export default class Header extends React.Component {
   render() {
-    return <header className="HeaderComponent">
-      <img className="HeaderComponent-logo" src={logoURL} height="125" />
+    return (
+      <header className="HeaderComponent">
+        <img className="HeaderComponent-logo" src={logoURL} height="125" />
 
-      <div className="HeaderComponent-wrap">
-        <h1 className="HeaderComponent-title">Tallstreet Starter Kit</h1>
-        <h2 className="HeaderComponent-tagline">The simple things tied together</h2>
-      </div>
-    </header>;
+        <div className="HeaderComponent-wrap">
+          <h1 className="HeaderComponent-title">Tallstreet Starter Kit</h1>
+          <h2 className="HeaderComponent-tagline">The simple things tied together</h2>
+        </div>
+      </header>
+    );
   }
 }
